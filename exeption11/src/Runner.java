e43e617907376eb036672b266c4fe92fbb8b1fdc
Kondratenko Andrey <anddrews@tut.by>
2015-11-11 22:47:47 +0300

    exception11

--- exeption11/src/Runner.java
+++ exeption11/src/Runner.java
@@ -1,21 +1,29 @@
-import by.gsu.epamlab.Model.PurchaseList;
+import by.gsu.epamlab.model.PurchaseList;
 
 public class Runner
 {
+    final static String FILE_EXTENSION=".csv";
+
     public static void main(String[] args)
     {
-        PurchaseList purchaseList=new PurchaseList("in.csv");
-        PurchaseList purchaseAddon=new PurchaseList("addon.csv");
+
+        final String IN_FILE_NAME=args[0]+FILE_EXTENSION;
+        final String ADDON_FILE_NAME=args[1]+FILE_EXTENSION;
+        final String COMPARATOR_NAME=args[2];
+        PurchaseList purchaseList=new PurchaseList(IN_FILE_NAME,COMPARATOR_NAME);
+        purchaseList.printBadRow();
+        PurchaseList purchaseAddon=new PurchaseList(ADDON_FILE_NAME,COMPARATOR_NAME);
+        purchaseAddon.printBadRow();
 
         purchaseList.printIntoTable();
-        purchaseList.addPurchases(0, purchaseAddon.getPurchase(purchaseAddon.getSize()-1));
+        purchaseList.addPurchases(0, purchaseAddon.getPurchase(purchaseAddon.getSize() - 1));
         purchaseList.addPurchases(1000, purchaseAddon.getPurchase(0));
         purchaseList.setPurchase(2, purchaseAddon.getPurchase(2));
         purchaseList.printIntoTable();
 
-           /* purchaseList.remove(3);
+            purchaseList.remove(3);
             purchaseList.remove(10);
-            purchaseList.remove(-5);*/
+            purchaseList.remove(-5);
 
         purchaseList.printIntoTable();
         purchaseList.sort();
